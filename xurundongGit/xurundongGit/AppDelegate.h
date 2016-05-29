17efe7d83e1e3a91943258ba8282db4f36316324
xurundong <1249145390@qq.com>
2016-05-29 23:13:54 +0800

    认识我欧胸襟

---
+++ xurundongGit/xurundongGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xurundongGit
+//
+//  Created by 徐润东 on 16/5/29.
+//  Copyright © 2016年 徐润东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xurundongGit/xurundongGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xurundongGit
+//
+//  Created by 徐润东 on 16/5/29.
+//  Copyright © 2016年 徐润东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
